7de6bcf953c761308f2a09fb7873a6bca9f52844
Bohassian <jacob.nmi.lee@gmail.com>
2019-10-01 10:46:40 -0500

    Adds routes for tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,65 @@
+class TasksController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    tasks = current_user.tasks.includes(:list_items)
+
+    render 'tasks/index', locals: { tasks: tasks }
+  end
+
+  def show
+    task = current_user.tasks.find_by(id: params[:id])
+
+    render 'tasks/show', locals: { task: task }
+  end
+
+  def new
+    task = current_user.tasks.new
+
+    render 'tasks/new', locals: { task: task }
+  end
+
+  def create
+    task = current_user.tasks.new(task_params)
+
+    if task.save
+      redirect_to task_path(task)
+    else
+      flash.now[:error] = "There was an error saving your task. Please review the form and try again"
+      render 'tasks/new', locals: { task: task }
+    end
+  end
+
+  def edit
+    task = current_user.tasks.find_by(id: params[:id])
+
+    render 'tasks/edit', locals: { task: task }
+  end
+
+  def update
+    task = current_user.tasks.find_by(id: params[:id])
+
+    if task.update_attributes(task_params)
+      redirect_to task_path(task)
+    else
+      flash.now[:error] = "There was an error saving your task. Please review the form and try again"
+      render 'tasks/edit', locals: { task: task }
+    end
+  end
+
+  def destroy
+    task = current_user.tasks.find_by(id: params[:id])
+
+    if !task.destroy
+      flash[:error] = "There was an error destroying your task. Please try again."
+    end
+
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name)
+  end
+end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :user, inverse_of: :tasks
 
-  has_many :list_items, inverse_of: :task
+  has_many :list_items, inverse_of: :task, dependent: :destroy
 
   validates :name, :user, presence: true
 end
